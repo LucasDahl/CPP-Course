cf4539d32f80b3d3547a10705c10d1f923275357
Lucas Dahl <LucasDahl425@yahoo.com>
2020-04-17 08:02:15 -0700

    Initial commit.

---
+++ CPP Course/quiz_7.cpp
@@ -0,0 +1,46 @@
+//
+//  quiz_7.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 4/17/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+//
+//  quiz_7.cpp
+//  C++Course
+//
+//  Created by Lucas Dahl on 4/17/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+// MARK: - Task
+/*
+Write a program called “Security System”, where you preset a string -type password.
+Your program should:
+1. Display a welcome message, and ask the user to enter the password. Example:
+“Welcome to the security system!  Please enter your password: ”
+2. If the user entered the right code, display:
+“Congratulations! Access granted! “
+“You will be directed to the homepage shortly…”
+If the user entered the wrong code, display:
+“Sorry, wrong code.”
+“Access denied.”
+“Please try again.”
+Upload your source code only.
+*/
+
+// Header
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    
+    
+    // Used to exit program
+    return 0;
+}
+
