c88ccc024be3e8b4dfc20dda42f40d20c4cdbf29
Lucas Dahl <LucasDahl425@yahoo.com>
2020-04-24 22:17:24 -0700

    Added some comments for testing.

--- CPP Course/Homework/Homework # 3/daysInAMonth.cpp
+++ CPP Course/Homework/Homework # 3/daysInAMonth.cpp
@@ -9,7 +9,7 @@
 // MARK: Tasks
 
 /*
- Write a program that asks the user to enter the month (letting the user enter an integer in the range of 1 through 12) and the year. The program should then display the number of days in that month. Use the following criteria to identify leap years: 
+ Write a program that asks the user to enter the month (letting the user enter an integer in the range of 1 through 12) and the year. The program should then display the number of days in that month. Use the following criteria to identify leap years:
  Determine whether the year is divisible by 100. If it is, then it is a leap year if and only if it is divisible by 400. For example, 2000 is a leap year but 2100 is not. (Note: Use % operator to check if a number is divisible by another. If the remainder is 0, it means divisible. i.e.( x % y == 0 ) means x is divisible by y. )
  If the year is not divisible by 100, then it is a leap year and if only it is divisible by 4. For example, 2008 is a leap year but 2009 is not.
  Here is a sample run of the program:
@@ -62,14 +62,14 @@ int main() {
                 
             case 2:
                 // Decision statement for leap year
-                if (year % 100 == 0) {
-                    if (year % 400 == 0) {
-                        cout << "29 days" << endl;
+                if (year % 4 == 0) {
+                    if (year % 4 == 0) { // only needs this I believe.
+                        cout << "29 days " << endl;
                     } else {
                         cout << "28 days" << endl;
                     }
                     
-                } else if (year % 100 != 0) {
+                } else if (year % 100 != 0) { // should be able to take this out
                     if (year % 4 == 0) {
                         cout << "29 days" << endl;
                     } else {
