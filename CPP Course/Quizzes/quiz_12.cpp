b38cb8a8c85d0606bf74374bd6a2c6844f45ca4b
Lucas Dahl <LucasDahl425@yahoo.com>
2020-04-28 12:11:12 -0700

    Finished all of homework #3, and added a file for quiz 12.

---
+++ CPP Course/Quizzes/quiz_12.cpp
@@ -0,0 +1,18 @@
+//
+//  quiz_12.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 4/28/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    
+//    
+//    // Used to exit the program
+//    return 0;
+//    
+//}
