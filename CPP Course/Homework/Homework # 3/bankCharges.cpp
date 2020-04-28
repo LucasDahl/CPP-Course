3d413455c2df2ee2e86621907a4789e1956286c3
Lucas Dahl <LucasDahl425@yahoo.com>
2020-04-28 09:08:16 -0700

    Finished the Bank charges assignment.

--- CPP Course/Homework/Homework # 3/bankCharges.cpp
+++ CPP Course/Homework/Homework # 3/bankCharges.cpp
@@ -10,29 +10,73 @@
 
 /*
  A bank charges $10 per month plus the following check fees for a commercial checking account:
-
+ 
  $.10 each for fewer than 20 checks
-
+ 
  $.08 each for 20 – 39 checks
-
+ 
  $.06 each for 40 -59 checks
-
+ 
  $.04 each for 60 or more checks
-
+ 
  The bank also charges an extra $15 if the balance of the account falls below $400 (before any check fees are applied). Write a program that asks for the beginning balance and the number of checks written. Compute and display the bank’s service fees for the month.
-
+ 
  Input Validation:
-
+ 
  Do not accept a negative value for the number of checks written.
  If a negative value is given for the beginning balance, display an urgent message indicating the account is overdrawn.
  */
 
-// Header
-//#include <iostream>
+// This program is let the user what the banks service charges are for the month.
 
-//int main() {
-//    
-//    // Used to exit the program
-//    return 0;
-//    
-//}
+// Header
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    
+    // Properties
+    float beginningBalance, monthlyCharge = 0.0; // Set to 0.0 to silence warnings
+    int numberOfChecks;
+    
+    // Ask the user for the number of checks and the staring balance of the account.
+    cout << "What is the starting balance of your account? ";
+    cin >> beginningBalance;
+    cout << "How many checks have you written? ";
+    cin >> numberOfChecks;
+    
+    
+    // format the data
+    if (numberOfChecks < 0) {
+        cout<< "Number of checks written must be\n" << "0 or more.\n";
+        cout << "Please rerun the program and try again.\n";
+    } else if (beginningBalance < 0) {
+        
+        cout << "Your account is over drawn..." << endl;
+        cout << "Please deposit money and try again." << endl;
+        
+    } else {
+        cout << setprecision(2) << fixed;
+        
+        if (numberOfChecks >= 0 && numberOfChecks < 20) {
+            monthlyCharge += (numberOfChecks * .1);
+        } else if (numberOfChecks >= 20 && numberOfChecks <= 39) {
+            monthlyCharge += (numberOfChecks * .08);
+        } else if (numberOfChecks >= 40 && numberOfChecks <= 59) {
+            monthlyCharge += (numberOfChecks * .06);
+        } else if (numberOfChecks >= 60) {
+            monthlyCharge += (numberOfChecks * .04);
+        }
+        
+        cout << "Bank's service fees for the month are: $";
+        cout << monthlyCharge << endl;
+        
+    }
+    
+    
+    // Used to exit the program
+    return 0;
+    
+}
--- CPP Course/Quizzes/quiz_11.cpp
+++ CPP Course/Quizzes/quiz_11.cpp
@@ -23,109 +23,109 @@
 // It will also let the user know if they need more players when there
 // is a partial or odd number of teams.
 
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    
-    // Properties
-    int teamSize = 0, totalPlayers = 0,// set to zero initially to silence warnings
-    numberOfTeams, leftoverPlayers;
-    string teamMessage = "How many players per team do you want? ",
-    playerMessage = "How many avaliable players are there? ",
-    teams;
-    bool flag = false, questionAsked = false;
-    
-    while (flag != true) {
-        
-        // Ask the user for the number of players and team size.
-        if (questionAsked == false) {
-            
-            cout << teamMessage;
-            cin >> teamSize;
-            cout << playerMessage;
-            cin >> totalPlayers;
-            
-            // Chang the flag var so the messages are asked differently
-            questionAsked = true;
-        }
-        
-        // The user had to many or to few players per team
-        while (teamSize <= 8 || teamSize >= 16) {
-            
-            if (teamSize <= 8) {
-                
-                cout << "You need at least 9 palyers per team...\n";
-                cout << teamMessage;
-                cin >> teamSize;
-                
-            } else if (teamSize >= 16) {
-                
-                cout << "You cannot have more than 15 players per team...\n";
-                cout << teamMessage;
-                cin >> teamSize;
-                
-            }
-            
-        }
-        
-        // User did not have enough players to even make one team.
-        while (totalPlayers <= 8) {
-            
-            // Not enough pleyers for even one team
-            cout << "Need more players...\n";
-            cout << playerMessage;
-            cin >> totalPlayers;
-            
-        }
-        
-        // Check if all of the criteria is met.
-        if ((teamSize >= 9 && teamSize <= 15) && totalPlayers >= 9) {
-            flag = true;
-        }
-        
-        
-    }// End while loop
-    
-    // Make the calculations.
-    // Not every combination will be an even.
-    numberOfTeams = (totalPlayers / teamSize);
-    leftoverPlayers = (totalPlayers % teamSize);
-    
-    // Check to see if teams is greater than one for correct grammer.
-    if (numberOfTeams > 1) {
-        teams = "teams";
-    } else {
-        teams = "team";
-    }
-    
-    cout << endl;
-    
-    // let the user know the results.
-    
-    if (leftoverPlayers == 0) {
-        
-        // Even number of players
-        cout << "With " << teamSize << " players per team, and "<< totalPlayers << " total players.\n";
-        cout << "You will have " << numberOfTeams << " " << teams << ".\n" << endl;
-        
-        
-    } else {
-        
-        
-        
-
-        // User has a partial team.
-        cout << "With " << teamSize << " players per team, and "<< totalPlayers << " total players.\n";
-        cout << "You will have " << numberOfTeams << " " << teams << ", with " << leftoverPlayers << " leftover.\n";
-        cout << "You may need more players for a fair game and tournaments...\n" << endl;
-        
-    }
-    
-    
-    
-    // Used to exit the program
-    return 0;
-    
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    
+//    // Properties
+//    int teamSize = 0, totalPlayers = 0,// set to zero initially to silence warnings
+//    numberOfTeams, leftoverPlayers;
+//    string teamMessage = "How many players per team do you want? ",
+//    playerMessage = "How many avaliable players are there? ",
+//    teams;
+//    bool flag = false, questionAsked = false;
+//    
+//    while (flag != true) {
+//        
+//        // Ask the user for the number of players and team size.
+//        if (questionAsked == false) {
+//            
+//            cout << teamMessage;
+//            cin >> teamSize;
+//            cout << playerMessage;
+//            cin >> totalPlayers;
+//            
+//            // Chang the flag var so the messages are asked differently
+//            questionAsked = true;
+//        }
+//        
+//        // The user had to many or to few players per team
+//        while (teamSize <= 8 || teamSize >= 16) {
+//            
+//            if (teamSize <= 8) {
+//                
+//                cout << "You need at least 9 palyers per team...\n";
+//                cout << teamMessage;
+//                cin >> teamSize;
+//                
+//            } else if (teamSize >= 16) {
+//                
+//                cout << "You cannot have more than 15 players per team...\n";
+//                cout << teamMessage;
+//                cin >> teamSize;
+//                
+//            }
+//            
+//        }
+//        
+//        // User did not have enough players to even make one team.
+//        while (totalPlayers <= 8) {
+//            
+//            // Not enough pleyers for even one team
+//            cout << "Need more players...\n";
+//            cout << playerMessage;
+//            cin >> totalPlayers;
+//            
+//        }
+//        
+//        // Check if all of the criteria is met.
+//        if ((teamSize >= 9 && teamSize <= 15) && totalPlayers >= 9) {
+//            flag = true;
+//        }
+//        
+//        
+//    }// End while loop
+//    
+//    // Make the calculations.
+//    // Not every combination will be an even.
+//    numberOfTeams = (totalPlayers / teamSize);
+//    leftoverPlayers = (totalPlayers % teamSize);
+//    
+//    // Check to see if teams is greater than one for correct grammer.
+//    if (numberOfTeams > 1) {
+//        teams = "teams";
+//    } else {
+//        teams = "team";
+//    }
+//    
+//    cout << endl;
+//    
+//    // let the user know the results.
+//    
+//    if (leftoverPlayers == 0) {
+//        
+//        // Even number of players
+//        cout << "With " << teamSize << " players per team, and "<< totalPlayers << " total players.\n";
+//        cout << "You will have " << numberOfTeams << " " << teams << ".\n" << endl;
+//        
+//        
+//    } else {
+//        
+//        
+//        
+//
+//        // User has a partial team.
+//        cout << "With " << teamSize << " players per team, and "<< totalPlayers << " total players.\n";
+//        cout << "You will have " << numberOfTeams << " " << teams << ", with " << leftoverPlayers << " leftover.\n";
+//        cout << "You may need more players for a fair game and tournaments...\n" << endl;
+//        
+//    }
+//    
+//    
+//    
+//    // Used to exit the program
+//    return 0;
+//    
+//}
