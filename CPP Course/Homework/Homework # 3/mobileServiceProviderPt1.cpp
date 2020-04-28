ef587f74c825bd2efe6858afa46027ee460b2d36
Lucas Dahl <LucasDahl425@yahoo.com>
2020-04-28 11:05:01 -0700

    Got the bulk of mobile service provider part 2 done.

--- CPP Course/Homework/Homework # 3/mobileServiceProviderPt1.cpp
+++ CPP Course/Homework/Homework # 3/mobileServiceProviderPt1.cpp
@@ -23,123 +23,123 @@
  */
 
 // Header
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int calculate() {
-    
-    // Properties
-    
-    // Constants
-    const float
-    PACKAGE_A = 39.99,
-    PACKAGE_B = 59.99,
-    PACKAGE_C = 69.99,
-    PACKAGE_A_MINUTES = 450,
-    PACKAGE_B_MINUTES = 900,
-    PACKAGE_A_ADDITIONAL_MINUTES = .45,
-    PACKAGE_B_ADDITIONAL_MINUTES = .4;
-    
-    // Variables
-    char userChoice;
-    float numberOfMinutes, totalCharges;
-  
-    // Ask the user what package they have.
-    cout << "What package do you have? ";
-    cin >> userChoice;
-    
-    // ask the user how many minutes they used.
-    cout << "How many minutes did you use? ";
-    cin >> numberOfMinutes;
-    
-    // Make sure user choice is lower.
-    tolower(userChoice);
-    
-    // Message part one
-    cout << "Total monthly charges are \n";
-    
-    switch (userChoice) {
-        case 'a':
-            
-            // Calculate the total charges
-            totalCharges =
-            numberOfMinutes > PACKAGE_A_MINUTES
-            ? PACKAGE_A + ((numberOfMinutes - PACKAGE_A_MINUTES) * PACKAGE_A_ADDITIONAL_MINUTES)
-            : PACKAGE_A;
-            
-            // Message part two.
-            cout << "$" << totalCharges  << " dollars.\n";
-            
-            break;
-        case 'b':
-            
-            // Calculate the total charges
-            totalCharges =
-            numberOfMinutes > PACKAGE_A_MINUTES
-            ? PACKAGE_B + ((numberOfMinutes - PACKAGE_B_MINUTES) * PACKAGE_B_ADDITIONAL_MINUTES)
-            : PACKAGE_B;
-            
-            // Message part two.
-            cout << "$" << totalCharges  << " dollars.\n";
-            
-            break;
-        case 'c':
-            
-            // Set the property.
-            totalCharges = PACKAGE_C;
-            
-            // Message part two.
-            cout << "$" << totalCharges  << " dollars.\n";
-            
-            break;
-            
-        default:
-            
-            // The user didnt pick a valid option
-            cout << "Not a valid option... please pick again.\n";
-            
-            // Reset total charges
-            totalCharges = 0;
-            numberOfMinutes = 0;
-            
-            // Restart the program.
-            calculate();
-            
-            break;
-    }
-    
-    
-    
-    
-    // Used to exit the program
-    return 0;
-}
-
-// The main method
-int main() {
-    
-    // Program name
-    cout << "Mobile Service monthly charge Calculator.\n";
-    
-    // Display the packages
-    cout << "\nPackage A: For $39.99 per month 450\n"
-    << "minutes are provided.\n"
-    << "Additional minutes are $0.45 per minute.\n"
-    << endl
-    << "Package B: For $59.99 per month 900\n"
-    << "minutes are provided.\n"
-    << "Additional minutes are $0.40 per minute.\n"
-    << endl
-    << "Package C: For $69.99 per month unlimited\n"
-    << "minutes provided.\n"
-    << endl;
-    
-    // Call the calculate method.
-    calculate();
-    
-    // Used to exit the program
-    return 0;;
-    
-}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int calculate() {
+//    
+//    // Properties
+//    
+//    // Constants
+//    const float
+//    PACKAGE_A = 39.99,
+//    PACKAGE_B = 59.99,
+//    PACKAGE_C = 69.99,
+//    PACKAGE_A_MINUTES = 450,
+//    PACKAGE_B_MINUTES = 900,
+//    PACKAGE_A_ADDITIONAL_MINUTES = .45,
+//    PACKAGE_B_ADDITIONAL_MINUTES = .4;
+//    
+//    // Variables
+//    char userChoice;
+//    float numberOfMinutes, totalCharges;
+//  
+//    // Ask the user what package they have.
+//    cout << "What package do you have? ";
+//    cin >> userChoice;
+//    
+//    // ask the user how many minutes they used.
+//    cout << "How many minutes did you use? ";
+//    cin >> numberOfMinutes;
+//    
+//    // Make sure user choice is lower.
+//    tolower(userChoice);
+//    
+//    // Message part one
+//    cout << "Total monthly charges are \n";
+//    
+//    switch (userChoice) {
+//        case 'a':
+//            
+//            // Calculate the total charges
+//            totalCharges =
+//            numberOfMinutes > PACKAGE_A_MINUTES
+//            ? PACKAGE_A + ((numberOfMinutes - PACKAGE_A_MINUTES) * PACKAGE_A_ADDITIONAL_MINUTES)
+//            : PACKAGE_A;
+//            
+//            // Message part two.
+//            cout << "$" << totalCharges  << " dollars.\n";
+//            
+//            break;
+//        case 'b':
+//            
+//            // Calculate the total charges
+//            totalCharges =
+//            numberOfMinutes > PACKAGE_A_MINUTES
+//            ? PACKAGE_B + ((numberOfMinutes - PACKAGE_B_MINUTES) * PACKAGE_B_ADDITIONAL_MINUTES)
+//            : PACKAGE_B;
+//            
+//            // Message part two.
+//            cout << "$" << totalCharges  << " dollars.\n";
+//            
+//            break;
+//        case 'c':
+//            
+//            // Set the property.
+//            totalCharges = PACKAGE_C;
+//            
+//            // Message part two.
+//            cout << "$" << totalCharges  << " dollars.\n";
+//            
+//            break;
+//            
+//        default:
+//            
+//            // The user didnt pick a valid option
+//            cout << "Not a valid option... please pick again.\n";
+//            
+//            // Reset total charges
+//            totalCharges = 0;
+//            numberOfMinutes = 0;
+//            
+//            // Restart the program.
+//            calculate();
+//            
+//            break;
+//    }
+//    
+//    
+//    
+//    
+//    // Used to exit the program
+//    return 0;
+//}
+//
+//// The main method
+//int main() {
+//    
+//    // Program name
+//    cout << "Mobile Service monthly charge Calculator.\n";
+//    
+//    // Display the packages
+//    cout << "\nPackage A: For $39.99 per month 450\n"
+//    << "minutes are provided.\n"
+//    << "Additional minutes are $0.45 per minute.\n"
+//    << endl
+//    << "Package B: For $59.99 per month 900\n"
+//    << "minutes are provided.\n"
+//    << "Additional minutes are $0.40 per minute.\n"
+//    << endl
+//    << "Package C: For $69.99 per month unlimited\n"
+//    << "minutes provided.\n"
+//    << endl;
+//    
+//    // Call the calculate method.
+//    calculate();
+//    
+//    // Used to exit the program
+//    return 0;;
+//    
+//}
