e091e8215e24830e94eb6fb0cfdbc15e355bec97
Lucas Dahl <LucasDahl425@yahoo.com>
2020-05-01 10:58:32 -0700

    Added some nested loop notes.

---
+++ CPP Course/Quizzes/quiz_14.cpp
@@ -0,0 +1,19 @@
+//
+//  quiz_14.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 5/1/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    
+    // Used to exit the program
+    return 0;
+    
+}
