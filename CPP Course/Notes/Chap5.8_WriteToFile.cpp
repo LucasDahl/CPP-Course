f4191bc7d400923b533c9aff8b614575dbc4e2d2
Lucas Dahl <LucasDahl425@yahoo.com>
2020-05-04 11:01:14 -0700

    Added some notes for writing files.

---
+++ CPP Course/Notes/Chap5.8_WriteToFile.cpp
@@ -0,0 +1,35 @@
+//// This program writes user input to a file.
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//    ofstream outputFile;
+//    string name1, name2, name3;
+//    
+//    // Open an output file.
+//    outputFile.open("Friends.txt");
+//    
+//    // Get the names of three friends.
+//    cout << "Enter the names of three friends.\n";
+//    cout << "Friend #1: ";
+//    cin >> name1;
+//    cout << "Friend #2: ";
+//    cin >> name2;
+//    cout << "Friend #3: ";
+//    cin >> name3;
+//    
+//    // Write the names to the file.
+//    outputFile << name1;
+//    outputFile << name2;
+//    outputFile << name3;
+//    cout << "The names were saved to a file.\n";
+//    
+//    // Close the file
+//    outputFile.close();
+//    
+//    system("pause");
+//    return 0;
+//}
--- CPP Course/Quizzes/quiz_14.cpp
+++ CPP Course/Quizzes/quiz_14.cpp
@@ -8,36 +8,36 @@
 
 // This program is used make a digital clock with nested loops.
 
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-int main() {
-    
-    
-    // Fromat the output.
-    cout << fixed << right;
-    cout.fill('0');
-    
-    // Loop for hours
-    for(int hours = 0; hours < 24; hours ++) {
-        
-        // Loop for minutes.
-        for(int minutes = 0; minutes < 60; minutes ++) {
-            
-            // Loop for seconds.
-            for(int seconds = 0; seconds < 60; seconds ++) {
-                
-                cout << setw(2) << hours << ":";
-                cout << setw(2) << minutes << ":";
-                cout << setw(2) << seconds << endl;
-                
-            }
-        }
-    }
-    
-    // Used to exit the program
-    return 0;
-    
-}
+//#include <iostream>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main() {
+//    
+//    
+//    // Fromat the output.
+//    cout << fixed << right;
+//    cout.fill('0');
+//    
+//    // Loop for hours
+//    for(int hours = 0; hours < 24; hours ++) {
+//        
+//        // Loop for minutes.
+//        for(int minutes = 0; minutes < 60; minutes ++) {
+//            
+//            // Loop for seconds.
+//            for(int seconds = 0; seconds < 60; seconds ++) {
+//                
+//                cout << setw(2) << hours << ":";
+//                cout << setw(2) << minutes << ":";
+//                cout << setw(2) << seconds << endl;
+//                
+//            }
+//        }
+//    }
+//    
+//    // Used to exit the program
+//    return 0;
+//    
+//}
