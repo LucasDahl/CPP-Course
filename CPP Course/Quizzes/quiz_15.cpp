026ede25db80b8e4926faa2cb865e3dbc01b3014
Lucas Dahl <LucasDahl425@yahoo.com>
2020-05-05 12:29:24 -0700

    Finished quiz 16.

--- CPP Course/Quizzes/quiz_15.cpp
+++ CPP Course/Quizzes/quiz_15.cpp
@@ -1,39 +1,40 @@
+////
+////  quiz_15.cpp
+////  CPP Course
+////
+////  Created by Lucas Dahl on 5/4/20.
+////  Copyright © 2020 Lucas Dahl. All rights reserved.
+////
 //
-//  quiz_15.cpp
-//  CPP Course
+//// This program writes user input to a file.
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//using namespace std;
 //
-//  Created by Lucas Dahl on 5/4/20.
-//  Copyright © 2020 Lucas Dahl. All rights reserved.
-//
-
-// This program writes user input to a file.
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-
-int main() {
-    
-    // Properties
-    ifstream inputFile;
-    string name1, name2, name3;
-    
-    // Link the file
-    inputFile.open("Friends.txt");
-    
-    // Display the file.
-    cout << "Now read from the file.\n";
-    inputFile >> name1;
-    inputFile >> name2;
-    inputFile >> name3;
-    
-    // Diaplay the names that were read from the file
-    cout << "Done reading. The names are:\n";
-    cout << name1 << ", " << name2 << ", and " << name3 << endl;
-    
-    
-    // Close the file
-    inputFile.close();
-    
-    return 0;
-}
+//int main() {
+//    
+//    // Properties
+//    ifstream inputFile;
+//    string name1, name2, name3;
+//    
+//    // Link the file
+//    inputFile.open("Friends.txt");
+//    
+//    // Display the file.
+//    cout << "Now read from the file.\n";
+//    // Use getLine to account for whitespace
+//    inputFile >> name1;
+//    inputFile >> name2;
+//    inputFile >> name3;
+//    
+//    // Diaplay the names that were read from the file
+//    cout << "Done reading. The names are:\n";
+//    cout << name1 << ", " << name2 << ", and " << name3 << endl;
+//    
+//    
+//    // Close the file
+//    inputFile.close();
+//    
+//    return 0;
+//}
---
+++ CPP Course/Quizzes/quiz_16.cpp
@@ -0,0 +1,66 @@
+//
+//  quiz_16.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 5/5/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+// This program is used to read from a file and display the total amount of numbers, the average of the numbers, and the sum total.
+
+// Header
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    // Properties
+    ifstream inputFile;
+    int num, totalNums = 0, sum = 0, average;
+    
+    // Open the file
+    inputFile.open("Random.txt");
+    
+    if (inputFile) {
+        
+        // Let the user know wht file the program is reading from.
+        cout << "From the text file Random.txt:" << endl;
+        
+        // Read from the file and display the data
+        while (inputFile >> num ) {
+  
+            
+            // Incrment per loop for total amount of numbers.
+            totalNums ++;
+            
+            // Add every number together for a sum.
+            sum += num;
+    
+    
+        } // End while
+        
+    } else {
+        
+        // Let the user know there was an error.
+        cout << "There was an error reading the file.. Please restart."
+        << endl;
+        
+    } // End if statment
+    
+    // Calculate the average
+    average = (sum / totalNums);
+    
+    // Display the data to the user.
+    cout << endl;
+    cout << "The total amount of numbers in the file is " <<totalNums << "." << endl;
+    cout << "The sum of all the numbers is " << sum << "." << endl;
+    cout << "The average value of all the numbers is " << average << "." << endl;
+    cout << endl;
+    
+    // Used to exit the program
+    return 0;
+    
+} // End main
