faaebb2473af1541c09e73a5695598ef335760a8
Lucas Dahl <LucasDahl425@yahoo.com>
2020-05-07 13:02:21 -0700

    Completed quiz 17.

--- CPP Course/Homework/Homework #4/Population Bar Chart.cpp
+++ CPP Course/Homework/Homework #4/Population Bar Chart.cpp
@@ -9,7 +9,7 @@
 // MARK: - Task
 
 /*
- Write a program that produces a bar chart showing the population growth of Prairieville, a small town in the Midwest, at 20-year intervals during the past 100 years. The program should read in the population figures (rounded to the nearest 1,000 people) for 1900, 1920, 1940, 1960, 1980, and 2000 from the file People.txt. For each year it should display the date and a bar consisting of one asterisk for each 1,000 people.
+ Write a program that produces a bar chart showing the population growth of Prairieville, a small town in the Midwest, at 20-year intervals during the past 100 years. The program should read in the population figures (rounded to the nearest 1,000 people) for 1900, 1920, 1940, 1960, 1980, 2000, and 2020 from the file People.txt. For each year it should display the date and a bar consisting of one asterisk for each 1,000 people.
 
  Here is an example of how the chart might begin:
 
@@ -24,63 +24,66 @@
  1940 *****
  */
 
+// This program is used to show the population growth for every 20 years
+// for the town of Prairieville.
 
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-int main() {
-    
-    // Properties
-    ifstream inputFile;
-    int num, year = 1900;
-    float numOfStars;
-    string population;
-
-    // Message to the user
-    cout << "\nPRAIRIEVILLE POPULATION GROWTH\n";
-    cout << "(each * represents 1,000 people)\n\n";
-
-    // OPen the file
-    inputFile.open("People.txt");
-    
-    if (inputFile) {
-        
-        while (inputFile >> num) {
-            
-            // Get the number of stars needed
-            numOfStars = num / 1000;
-            
-            // Reset the population string for the new line.
-            population = "";
-            
-            for(int i = 0; i < numOfStars; i ++) {
-                
-                population += "*";
-                
-            }
-            
-            // Display the message to the user.
-            cout << year << " " << population << endl;
-            
-            // Add 20 years
-            year += 20;
-            
-        }
-        
-    } else {
-        
-        // COuldnt open or read the file.
-        cout << "Error opening the file, please try again." << endl;
-        
-    }
-    
-    // Close the file
-    inputFile.close();
-
-    // Used to exit the program
-    cout << endl;
-    return 0;
-    
-}
+// Header
+//#include <iostream>
+//#include <fstream>
+//
+//using namespace std;
+//
+//int main() {
+//    
+//    // Properties
+//    ifstream inputFile;
+//    int num, year = 1900;
+//    float numOfStars;
+//    string population;
+//
+//    // Message to the user
+//    cout << "\nPRAIRIEVILLE POPULATION GROWTH\n";
+//    cout << "(each * represents 1,000 people)\n\n";
+//
+//    // OPen the file
+//    inputFile.open("People.txt");
+//    
+//    if (inputFile) {
+//        
+//        while (inputFile >> num) {
+//            
+//            // Get the number of stars needed
+//            numOfStars = num / 1000;
+//            
+//            // Reset the population string for the new line.
+//            population = "";
+//            
+//            for(int i = 0; i < numOfStars; i ++) {
+//                
+//                population += "*";
+//                
+//            }
+//            
+//            // Display the message to the user.
+//            cout << year << " " << population << endl;
+//            
+//            // Add 20 years
+//            year += 20;
+//            
+//        }
+//        
+//    } else {
+//        
+//        // COuldnt open or read the file.
+//        cout << "Error opening the file, please try again." << endl;
+//        
+//    }
+//    
+//    // Close the file
+//    inputFile.close();
+//
+//    // Used to exit the program
+//    cout << endl;
+//    return 0;
+//    
+//}
---
+++ CPP Course/Quizzes/quiz_17.cpp
@@ -0,0 +1,44 @@
+//
+//  quiz_17.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 5/7/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+// Use protoype methods, see pics.
+
+#include <iostream>
+using namespace std;
+
+// Prototype functions.
+void print_star();
+void print_message();
+
+// Mian function
+int main() {
+    
+    // Call the methods to produce the message.
+    print_star();
+    print_message();
+    print_star();
+    
+    // Used to end the program
+    cout << endl;
+    return 0;
+    
+}
+
+// Function prints a line of stars.
+void print_star() {
+    
+    cout << "* * * * * * * * * *";
+    
+}
+
+// Function prints a message
+void print_message() {
+    
+    cout << "\n\n  How do you do!\n\n";
+    
+}
