2687aa7506d188166ed3fad64285eda297037ef6
Lucas Dahl <LucasDahl425@yahoo.com>
2020-05-18 14:11:09 -0700

    Finished quiz 21.

---
+++ CPP Course/Notes/ArrayNotes.cpp
@@ -0,0 +1,31 @@
+//
+//  ArrayNotes.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 5/18/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    
+//    // Properties
+//    const int SIZE = 10;
+//    int num [SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//    string names[SIZE];
+//    
+//    // Access the array
+//    cout << num[2] << endl;
+//    
+//    // Use a for loop
+//    for(int i = 0; i < SIZE; i++) {
+//        cout << num[i] << endl;
+//    }
+//    
+//    // Exit the program
+//    return 0;
+//    
+//}
---
+++ CPP Course/Quizzes/quiz_21.cpp
@@ -0,0 +1,37 @@
+//
+//  quiz_21.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 5/18/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    
+//    // Properties
+//    const int NUM_FISH = 20;
+//    int fish[NUM_FISH];
+//    
+//    // Ask for the number of fish per fishermen.
+//    for(int i = 0; i < NUM_FISH; i++) {
+//        
+//        cout << "How many fish did fishermen number: " << i + 1 << " catch? " << endl;
+//        cin >> fish[i];
+//        
+//    }
+//    
+//    // Display the array for all the fish caught by the fishermen.
+//    for(int i = 0; i < NUM_FISH; i++) {
+//        
+//        cout << "Fishermen " << i + 1 << " caught " << fish[i] << endl;
+//        
+//    }
+//    
+//    // Used to exit the program.
+//    return 0;
+//    
+//}
