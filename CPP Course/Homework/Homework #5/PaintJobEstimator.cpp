39ea0f005d0d1a1a5f612762dbda3103e0d70482
Lucas Dahl <LucasDahl425@yahoo.com>
2020-05-20 12:22:33 -0700

    Completed the paint job estimator.

--- CPP Course/Homework/Homework #5/PaintJobEstimator.cpp
+++ CPP Course/Homework/Homework #5/PaintJobEstimator.cpp
@@ -9,21 +9,21 @@
 // MARK: - Task
 /*
  A painting company has determined that for every 110 square feet of wall space, 1 gallon of paint and 8 hours of labor will be required. The company charges $25.00 per hour for labor.
-
+ 
  Write a modular program (a program that is divided into functions that perform each major task) that allows the user to enter
-
+ 
  the number of rooms that are to be painted and
  the price of the paint per gallon.
  It should also ask for the square feet of wall space in each room. (The wall space for a standard room is about 340 square feet.)
  It should then display the following data:
-
+ 
  The number of gallons of paint required
  The hours of labor required
  The cost of the paint
  The labor charges
  The total cost of the paint job.
  Input validation: Do not accept:
-
+ 
  A value less than 1 for the number of rooms
  A value less than $10.00 for the price of paint
  A negative value for square footage of wall space
@@ -31,111 +31,109 @@
 
 // 340 square feet as min
 
-//#include <iostream>
-//using namespace std;
-//
-//double inputValidate(double, double);
-//void getInfo(double &, double &, double &);
-//void calcAndDisplay(double, double, double);
-//
-//int main()
-//{
-//    const double CHARGE_PER_HOUR = 25.00;
-//    double num_of_rooms,
-//           price_per_gal,
-//           sqft_to_paint = 0;
-//
-//    getInfo(num_of_rooms, price_per_gal, sqft_to_paint);
-//    calcAndDisplay(num_of_rooms, price_per_gal, sqft_to_paint);
-//
-//    return 0;
-//} // END int main()
-//
-//void getInfo(double &num_of_rooms,
-//             double &price_per_gal,
-//             double &sqft_to_paint)
-//{
-//     // Get number of rooms to be painted
-//     cout << "Number of rooms to be painted: ";
-//     num_of_rooms = inputValidate(num_of_rooms, 1);
-//
-//     for (int i = 0; i < num_of_rooms; i++)
-//     {
-//          // Get sqft of wall space per room
-//          cout << "Sq. Ft. of wall space to be painted "
-//               << "in room " << (i + 1) << ": ";
-//
-//          sqft_to_paint += inputValidate(sqft_to_paint, 0);
-//     }
-//
-//     // Get price of paint per gallon
-//     cout << "Price of paint per gallon: $";
-//     price_per_gal = inputValidate(price_per_gal, 10.00);
-//
-//}
-//
-//double inputValidate(double number, double limit_number)
-//{
-//    
-//    while(!(cin >> number) || number < limit_number)
-//    {
-//        cout << "Error. Number must not be "
-//             << " " << limit_number << " or greater:";
-//        cin.clear();
-//        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-//    }
-//
-//    return number;
-//}
-//
-//void calcAndDisplay(double num_of_rooms,
-//                    double price_per_gal,
-//                    double sqft_to_paint)
-//{
-//     double gals_of_paint_req,
-//            labor_required,
-//            cost_of_paint,
-//            labor_charges,
-//            total_cost;
-//
-//     // Calculate:
-//     gals_of_paint_req  = sqft_to_paint / 110.0;
-//     labor_required = gals_of_paint_req * 8.0;
-//     cost_of_paint  = price_per_gal * gals_of_paint_req;
-//     labor_charges  = labor_required * 25.00;
-//     total_cost     = labor_charges + cost_of_paint;
-//
-//     // Display:
-//     cout << "Total SqFt to paint  : "
-//          << sqft_to_paint
-//          << endl;
-//
-//     cout << "Price per gallon     : "
-//          << price_per_gal
-//          << endl;
-//
-//     // • The number of gallons of paint required
-//     cout << "Gallons required     : "
-//          << gals_of_paint_req
-//          << endl;
-//
-//     // • The hours of labor required
-//     cout << "Hours required       : "
-//          << labor_required
-//          << endl;
-//
-//     // • The cost of the paint
-//     cout << "Cost of paint        :$"
-//          << cost_of_paint
-//          << endl;
-//
-//     // • The labor charges
-//     cout << "Labor charges        :$"
-//          << labor_charges
-//          << endl;
-//
-//     // • The total cost of the paint job
-//     cout << "Total cost of job    :$"
-//          << total_cost
-//          << endl;
-//}
+// Header
+#include <iostream>
+using namespace std;
+
+// Prototype functions.
+double inputValidate(double, double);
+void getInfo(double&, double&, double&);
+void calcDisplay(double, double, double);
+
+// Main method.
+int main() {
+    
+    // Properties
+    double numOfRooms, pricePerGal, sqftToPaint = 0;
+    
+    // Get all the info
+    getInfo(numOfRooms, pricePerGal, sqftToPaint);
+    
+    // Display rhe data after calculation
+    calcDisplay(numOfRooms, pricePerGal, sqftToPaint);
+    
+    // Used to exit the program.
+    cout << endl;
+    return 0;
+    
+}
+
+// Gather all the info
+void getInfo(double& numOfRooms, double& pricePerGal, double& sqftToPaint) {
+    
+    // Get number of rooms to be painted
+    cout << "Number of rooms to be painted: ";
+    
+    // Validate the input
+    numOfRooms = inputValidate(numOfRooms, 1);
+    
+    // Ask for the square ft for each room.
+    for (int i = 0; i < numOfRooms; i++) {
+        
+        // Get sqft of wall space per room
+        cout << "Sq. Ft. of wall space to be painted " << "in room " << (i + 1) << ": ";
+        
+        // Check for valid input
+        sqftToPaint += inputValidate(sqftToPaint, 0);
+        
+    }
+    
+    // Get price of paint per gallon
+    cout << "Price of paint per gallon: $";
+    
+    // Check for valid input
+    pricePerGal = inputValidate(pricePerGal, 10.00);
+    
+}
+
+// Check for valid input.
+double inputValidate(double number, double limitNumber) {
+    
+    // Make sure the input is valid
+    while(!(cin >> number) || number < limitNumber) {
+        
+        cout << "Error. Number must not be" << " " << limitNumber << " or greater: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        
+    }
+    
+    // Return the number
+    return number;
+    
+}
+
+// Calculate and display the data
+void calcDisplay(double numOfRooms, double pricePerGal, double sqftToPaint) {
+    
+    // Properties
+    double galsOfPaintReq, laborRequired, costOfPaint, laborCharges, totalCost;
+    
+    // Calculate
+    galsOfPaintReq = sqftToPaint / 110.0;
+    laborRequired = galsOfPaintReq * 8.0;
+    costOfPaint = pricePerGal * galsOfPaintReq;
+    laborCharges = laborRequired * 25.00;
+    totalCost = laborCharges + costOfPaint;
+    
+    // Display:
+    cout << "Total SqFt to paint  : " << sqftToPaint << endl;
+    
+    cout << "Price per gallon     : " << pricePerGal << endl;
+    
+    // The number of gallons of paint required
+    cout << "Gallons required     : " << galsOfPaintReq << endl;
+    
+    // The hours of labor required
+    cout << "Hours required       : " << laborRequired << endl;
+    
+    // The cost of the paint
+    cout << "Cost of paint        :$" << costOfPaint << endl;
+    
+    // The labor charges
+    cout << "Labor charges        :$" << laborCharges << endl;
+    
+    // The total cost of the paint job
+    cout << "Total cost of job    :$" << totalCost << endl;
+    
+}
---
+++ CPP Course/Quizzes/quiz_23.cpp
@@ -0,0 +1,20 @@
+//
+//  quiz_23.cpp
+//  CPP Course
+//
+//  Created by Lucas Dahl on 5/20/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+//// Header
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main() {
+//    
+//    
+//    // Used to exit the program.
+//    return 0;
+//    
+//}
