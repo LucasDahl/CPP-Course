1525d6ba3ba807ffb26397491a15e613e615f994
Lucas Dahl <LucasDahl425@yahoo.com>
2020-06-09 07:04:51 -0700

    Finished the reverse array assignment.

--- CPP Course/Homework/Homework #7/ReverseArray.cpp
+++ CPP Course/Homework/Homework #7/ReverseArray.cpp
@@ -12,14 +12,67 @@
  The function should return a pointer to the new array. Demonstrate the function in a complete program.
  */
 
+//Header
 #include <iostream>
 using namespace std;
 
+// Prototype functions
+int *reverse(int[], int);
+void print(int[], int);
 
+// Main function
 int main() {
     
+    // Properties
+    int size = 10, array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },*reversedArray = reverse(array, size);
     
-    // Return
+    // Print the original array
+    cout << "\nThe original array is" << endl;
+    print(array, size);
+    
+    // Print the reverse array
+    cout << "\nReversed array" << endl;
+    print(reversedArray, size);
+    
+    // Used to exit the program
     return 0;
+    
+}
+
+// Reverse the arry
+int *reverse(int array[],int size) {
+    
+    // Proerties
+    int *copyReverse = new int[size];
+    
+    // Check the size
+    if(size <= 0) {
+        return NULL;
+    }
+        
+    // Loop through and add to the array
+    for(int i = 0; i < size; i++) {
+        
+        copyReverse[i] = array[size -i -1];
+        
+    }
+    
+    // Return the array
+    return copyReverse;
+    
+}
+
+
+// Print the array
+void print(int array[],int n) {
+    
+    // Loop through each element and print it
+    for(int i = 0; i < n; i++) {
+        
+        cout << array[i] << " ";
+    }
+    
+    // Add a space
+    cout << endl;
     
 }
